aa5d7ba646bcca12f84e05d2fb2f6af66eb3fd6a
Christopher Montgomery <krizzay95@gmail.com>
2023-02-14 19:17:04 -0700

    Changed colors, added date

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ document.addEventListener("DOMContentLoaded", function () {
   const form = document.querySelector("#to-do-form");
   const input = document.querySelector("#to-do-input");
   const list = document.querySelector("#to-do-list");
+  const n = new Date();
+  const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+  const y = n.getFullYear();
+  const m = month[n.getMonth()];
+  const d = weekday[n.getDay()];
+  const d2 = n.getDate();
+  document.getElementById("date").innerHTML = d + " " + m + " " + d2 + "," + " " + y;
 
   form.addEventListener("submit", function (event) {
     event.preventDefault();
